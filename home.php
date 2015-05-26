4b27e99d000b9925cc7b38edc378e5d2419c61fa
Lingduo Kong <kkklllddd@uchicago.edu>
2015-05-25 23:40:09 -0500

    new

---
+++ home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/icon.png">
+        <title>Sample</title>
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap-dropdown.js"></script>
+        <style>
+        h1, h2, h3{color:#A30000}
+        h1{font-size: 50px}
+        </style>
+    </head>
+    <body>
+        <?php include 'php/nav.php';?>
+
+        <div class="container" style="padding-top:110px">
+            <div class="row"><h2 style="text-align:center">The first blank line!</h2></div><br>
+            <div class="col-xs-9">
+                <div class="jumbotron">
+                    <h2>picture</h2>
+                </div>
+                <div class="jumbotron">
+                    <h2>get started</h2>
+                </div>
+                <div class="jumbotron">
+                    <h2>details</h2>
+                </div>
+            </div>
+            <div class="col-xs-3">
+                <div class="jumbotron" >
+                    <h3>what is new</h3>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </body>
---
+++ php/nav.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="home.php">
+                <div style="font-size: 3em; padding-top:30px">Sample</div>
+            </a>
+        </div>
+        <div>
+            <ul class="nav navbar-nav " style="padding-top:50px">
+                <li><a href="">downloads</a></li>
+                <li><a href="">documentations</a></li>
+                <li><a href="">case Studies</a></li>
+                <li><a href="">papers</a></li>
+                <li><a href="">support</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
